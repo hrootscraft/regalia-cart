a103616a7a96c71160a1fbe049278edd0c950b05
Rutuja <rutuja1675@gmail.com>
2022-07-20 09:20:18 +0530

    add home screen

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "Cabinet with glass doors",
+    image: "/images/white-cabinet.png",
+    description:
+      "Stylish design that matches your existing decor. This glass and mesh display cabinet is simplicity at its best and makes eye-catchers of your finest things.",
+    brand: "Ikea",
+    category: "Home Decor",
+    price: 3900.00,
+    countInStock: 10,
+    rating: 4.2,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Work lamp",
+    image: "/images/work-lamp.png",
+    description:
+      "Work lamp was introduced in our range in 1998. The classic, steel design with adjustable arm and head makes it a perfect choice if you are looking for a flexible and effective reading light.",
+    brand: "Ikea",
+    category: "Home Decor",
+    price: 1089.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Pendant lamp shade",
+    image: "/images/pendant-lamp-shade.png",
+    description:
+      "You can easily transform this neat, airy lampshade into a personal pendant lamp with a matching cord set and a decorative LED bulb. Perfect to hang over the dining table or in a row in the hallway.",
+    brand: "Cannon",
+    category: "Home Decor",
+    price: 990.00,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Mirror",
+    image: "/images/mirror.png",
+    description:
+      "Its the curly, swirly steel wire that makes this mirror so special. This simple detail adds a decorative touch that catches your eye.",
+    brand: "Cannon",
+    category: "Home Decor",
+    price: 999.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Floor uplighter",
+    image: "/images/floor-uplighter.png",
+    description:
+      "Floor uplighter illuminates upwards and spreads a diffused, pleasant light around a room. Parts of the lamp are also made of recycled plastic a good choice for our climate at an unbeatable price.",
+    brand: "Amazon",
+    category: "Home Decor",
+    price: 949.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "Artifi potted plant and pot",
+    image: "/images/potted-plants.png",
+    description:
+      "Artificial potted plants that don't require a green thumb. Perfect when you have better things to do than water plants and tidy up dead leaves. You'll have everyone fooled because they look so lifelike.",
+    brand: "Amazon",
+    category: "Home Decor",
+    price: 199.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
