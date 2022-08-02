619848a353de59c8a786bcf93f0e5be5f30c1e35
Rutuja <rutuja1675@gmail.com>
2022-08-02 21:03:52 +0530

    add dot env

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 const app = express();
 import products from "./data/products.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("API is running...");
@@ -15,4 +18,9 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
