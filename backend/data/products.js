0cba9a830c3e719ea608cd8353a904c9a7fe6d42
Rutuja <rutuja1675@gmail.com>
2022-08-04 08:09:30 +0530

    prepare sample data

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,19 +1,17 @@
 const products = [
   {
-    _id: "1",
     name: "Cabinet with glass doors",
     image: "/images/white-cabinet.png",
     description:
       "Stylish design that matches your existing decor. This glass and mesh display cabinet is simplicity at its best and makes eye-catchers of your finest things.",
     brand: "Ikea",
     category: "Home Decor",
-    price: 3900.00,
+    price: 3900.0,
     countInStock: 0,
     rating: 4.6,
     numReviews: 12,
   },
   {
-    _id: "2",
     name: "Work lamp",
     image: "/images/work-lamp.png",
     description:
@@ -26,20 +24,18 @@ const products = [
     numReviews: 8,
   },
   {
-    _id: "3",
     name: "Pendant lamp shade",
     image: "/images/pendant-lamp-shade.png",
     description:
       "You can easily transform this neat, airy lampshade into a personal pendant lamp with a matching cord set and a decorative LED bulb. Perfect to hang over the dining table or in a row in the hallway.",
     brand: "Cannon",
     category: "Home Decor",
-    price: 990.00,
+    price: 990.0,
     countInStock: 5,
     rating: 3,
     numReviews: 12,
   },
   {
-    _id: "4",
     name: "Mirror",
     image: "/images/mirror.png",
     description:
@@ -52,7 +48,6 @@ const products = [
     numReviews: 12,
   },
   {
-    _id: "5",
     name: "Floor uplighter",
     image: "/images/floor-uplighter.png",
     description:
@@ -65,7 +60,6 @@ const products = [
     numReviews: 10,
   },
   {
-    _id: "6",
     name: "Artifi potted plant and pot",
     image: "/images/potted-plants.png",
     description:
