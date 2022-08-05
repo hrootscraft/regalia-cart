9af1e20c7191db3c9dd1c6613d0d8d25dbace875
Rutuja <rutuja1675@gmail.com>
2022-08-05 10:41:49 +0530

    add custom authentication middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import asyncHandler from "express-async-handler";
-import { authUser } from "../controllers/userController.js";
+import { authUser, getUserProfile } from "../controllers/userController.js";
 import express from "express";
 const router = express.Router();
+import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/login").post(authUser);
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
