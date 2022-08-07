46d862a5a2819f8ce55744901d379b4474d8091a
Rutuja <rutuja1675@gmail.com>
2022-08-07 12:41:43 +0530

    update to paid endpt

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
 const router = express.Router();
-import { addOrderItems, getOrderById } from "../controllers/orderController.js";
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
+router.route("/:id:pay").put(protect, updateOrderToPaid);
 router.route("/:id").get(protect, getOrderById);
 router.route("/").post(protect, addOrderItems);
 
