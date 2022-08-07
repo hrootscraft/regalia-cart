5e589804297c96f963f40e215aa5268efdaae3da
Rutuja <rutuja1675@gmail.com>
2022-08-07 12:30:08 +0530

    create order screen

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 import { addOrderItems, getOrderById } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
-router.route("/").post(protect, addOrderItems);
 router.route("/:id").get(protect, getOrderById);
+router.route("/").post(protect, addOrderItems);
 
 export default router;
