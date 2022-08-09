85801199280b103b731ff8c5fb2a88de520d37eb
Rutuja <rutuja1675@gmail.com>
2022-08-09 15:09:05 +0530

    add order pay reducer, acion & paypal payments

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,23 +3,18 @@ import {
   combineReducers,
   applyMiddleware,
 } from "redux";
-
 import thunk from "redux-thunk";
-
 import { composeWithDevTools } from "redux-devtools-extension";
-
 import {
   productListReducer,
   productDetailsReducer,
 } from "./reducers/productReducers";
-
 import { cartReducer } from "./reducers/cartReducers";
-
 import {
   orderCreateReducer,
   orderDetailsReducer,
+  orderPayReducer,
 } from "./reducers/orderReducers";
-
 import {
   userLoginReducer,
   userRegisterReducer,
@@ -37,6 +32,7 @@ const reducer = combineReducers({
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
