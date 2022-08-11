76476da7dcee28058978fcb746b633baacdcf3b9
Rutuja <rutuja1675@gmail.com>
2022-08-11 16:18:22 +0530

    production ready

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use("/api/products", productRoutes);
 
 app.use("/api/users", userRoutes);
@@ -38,10 +34,24 @@ app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYMENT_CLIENT_ID)
 );
 
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  // every route that is not our api end pt. is rendered with index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "/frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
 // the uploads folder won't be accessible in the browser,
 // we need to make it static so that it can get loaded in the browser,
 // which is done in express as follows:
-const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
