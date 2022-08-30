d5345251b6475943b1f7db261efe6a0b4b889cc3
Rutuja <rutuja1675@gmail.com>
2022-08-30 19:18:18 +0530

    fix a typo

--- backend/server.js
+++ backend/server.js
@@ -35,24 +35,23 @@ app.get("/api/config/paypal", (req, res) =>
 );
 
 const __dirname = path.resolve();
+// the uploads folder won't be accessible in the browser,
+// we need to make it static so that it can get loaded in the browser,
+// which is done in express as follows:
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   // every route that is not our api end pt. is rendered with index.html
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "/frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
     res.send("API is running...");
   });
 }
-
-// the uploads folder won't be accessible in the browser,
-// we need to make it static so that it can get loaded in the browser,
-// which is done in express as follows:
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 app.use(errorHandler);
